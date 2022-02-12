c4665a330a9ba465b439dfc147a2887329f23de2
yahav <yahav@cloudteam.ai>
2022-02-12 15:09:51 +0200

    test

--- eks/src/main.tf
+++ eks/src/main.tf
@@ -1,6 +1,9 @@
 terraform {
   required_version = ">=0.14.8"
   backend "s3" {
+       bucket = "cloudteam-tf-circles"
+       key    = "Terraform/circlesup/eks"
+       region = "eu-west-3"
   }
    required_providers {
     aws = {
